042d75da85dd823465f94addecc72600cc1ada63
Sebastian Rutofski <sebastian.rutofski@gmail.com>
2018-07-04 11:00:17 +0200

    fix #4
    converts loose functions to a RedditWertpapierBot holder class

--- main.py
+++ main.py
@@ -57,45 +57,13 @@ else:
     logger.setLevel(logging.DEBUG)
 logger.addHandler(handler)
 
-reddit = None
-subreddit = None
-
-
-def setup_reddit():
-    global reddit
-    global subreddit
-    # authenticate against reddit api and obtain an Reddit instance and ref to finanzen subreddit
-    # get configuration from praw.INI if existing else try getting data from env vars
-    if Path("praw.INI").is_file():
-        logger.info("Getting new reddit instance using data from praw.INI")
-        reddit = praw.Reddit("wertpapierbot", user_agent=USER_AGENT)
-    else:
-        # TODO: read env vars
-        if False:
-            logger.info("Getting new reddit instance using data from environment variables")
-            # TODO: set up reddit instance
-        else:
-            logger.error("No configuration found")
-            exit(-1)
-    # basic reddit instance checks
-    if not reddit:
-        logger.error("Reddit instance is not initialized")
-        exit(-1)
-    if reddit.read_only:
-        logger.error("Reddit instance is read only")
-        exit(-1)
-    # get subreddit for processing
-    subreddit = reddit.subreddit("finanzen")
-    if not subreddit:
-        logger.error("Subreddit instance is not initialized")
-        exit(-1)
-
 
 def get_fund_data(identifier):
     url = DATA_URL + identifier
     values = {}
     response = requests.get(url, headers={'Accept-Language ': 'de-DE', 'User-Agent': USER_AGENT},
-                            cookies={'DisplayUniverse': 'DE-priv', 'PreferredLanguage': 'de', 'PrivacyPolicy': 'true',
+                            cookies={'DisplayUniverse': 'DE-priv', 'PreferredLanguage': 'de',
+                                     'PrivacyPolicy': 'true',
                                      'DisclaimerAccepted': 'true'})
     if response.status_code != 200:
         logger.error("An error occurred while fetching %s: %s", url, response.status_code)
@@ -139,74 +107,108 @@ def get_fund_data(identifier):
     return values
 
 
-# handle a single submission including all comments
-def handle_stock_requests(comment, matches):
-    message = ""
-    for match in matches:
-        try:
-            values = get_fund_data(match)
-            if values:
-                message = message + FUND_INFO_STRING.format(**values)
-        except Exception as e:
-            logger.error(
-                "An error occurred while gathering funds data for \"%s\": %s", match, repr(e))
-    message = message + BOT_DISCLAIMER
-    # reply = reddit.comment(comment).reply(message)
-    # logger.info("Replied to %s, reply id: %s", comment, reply.id)
+class RedditWertpapierBot:
+    def __init__(self):
+        self.__reddit = None
+        self.__subreddit = None
 
+    def __setup_reddit(self):
+        # authenticate against reddit api and obtain an Reddit instance and ref to finanzen subreddit
+        # get configuration from praw.INI if existing else try getting data from env vars
+        if Path("praw.INI").is_file():
+            logger.info("Getting new reddit instance using data from praw.INI")
+            self.__reddit = praw.Reddit("wertpapierbot", user_agent=USER_AGENT)
+        else:
+            # TODO: read env vars
+            if False:
+                logger.info("Getting new reddit instance using data from environment variables")
+                # TODO: set up reddit instance
+            else:
+                logger.error("No configuration found")
+                exit(-1)
+        # basic reddit instance checks
+        if not self.__reddit:
+            logger.error("Reddit instance is not initialized")
+            exit(-1)
+        if self.__reddit.read_only:
+            logger.error("Reddit instance is read only")
+            exit(-1)
+        # get subreddit for processing
+        self.__subreddit = reddit.subreddit("finanzen")
+        if not self.__subreddit:
+            logger.error("Subreddit instance is not initialized")
+            exit(-1)
 
-def handle_submission(sub):
-    submission = reddit.submission(id=sub)
-    submission.comment_sort = "new"
-    submission.comments.replace_more(limit=0)
+    # handle a single submission including all comments
+    def __handle_stock_requests(self, comment, matches):
+        message = ""
+        for match in matches:
+            try:
+                values = get_fund_data(match)
+                if values:
+                    message = message + FUND_INFO_STRING.format(**values)
+            except Exception as e:
+                logger.error(
+                    "An error occurred while gathering funds data for \"%s\": %s", match, repr(e))
+        message = message + BOT_DISCLAIMER
+        # reply = self.reddit.comment(comment).reply(message)
+        # logger.info("Replied to %s, reply id: %s", comment, reply.id)
 
-    bot_replied = False
-    comment_queue = submission.comments[:]
+    def __handle_submission(self, sub):
+        submission = self.__reddit.submission(id=sub)
+        submission.comment_sort = "new"
+        submission.comments.replace_more(limit=0)
 
-    while comment_queue:
-        com = comment_queue.pop(0)
-        logger.debug("Parsing comment %s", com.id)
+        bot_replied = False
+        comment_queue = submission.comments[:]
 
-        if not com.author:
-            continue
+        while comment_queue:
+            com = comment_queue.pop(0)
+            logger.debug("Parsing comment %s", com.id)
 
-        # add sub comments to queue
-        comment_queue.extend(com.replies)
+            if not com.author:
+                continue
 
-        # check if the comment is a top level comment and by the bot
-        if not bot_replied and com.depth == 0 and com.author.name == "WertpapierBot":
-            bot_replied = True
+            # add sub comments to queue
+            comment_queue.extend(com.replies)
 
-        com_body = com.body
+            # check if the comment is a top level comment and by the bot
+            if not bot_replied and com.depth == 0 and com.author.name == "WertpapierBot":
+                bot_replied = True
 
-        match_results = list()
-        match_results.extend(WKN_PATTERN.findall(com_body))
-        match_results.extend(ISIN_PATTERN.findall(com_body))
-        if match_results:
-            logger.debug("Stock ids found: %s", match_results)
-            # TODO: check for existing bot reply
-            responded = False
-            for rep in com.replies:
-                if isinstance(rep, MoreComments):
-                    continue
-                if rep.author.name == "WertpapierBot":
-                    responded = True
-                    break
-            if not responded:
-                handle_stock_requests(com, match_results)
+            com_body = com.body
 
+            match_results = list()
+            match_results.extend(WKN_PATTERN.findall(com_body))
+            match_results.extend(ISIN_PATTERN.findall(com_body))
+            if match_results:
+                logger.debug("Stock ids found: %s", match_results)
+                # TODO: check for existing bot reply
+                responded = False
+                for rep in com.replies:
+                    if isinstance(rep, MoreComments):
+                        continue
+                    if rep.author.name == "WertpapierBot":
+                        responded = True
+                        break
+                if not responded:
+                    self.__handle_stock_requests(com, match_results)
 
-def main_loop():
-    logger.info("Main loop started")
-    while True:
-        # parse the last 25 submissions in /r/finanzen
-        for sub in subreddit.new(limit=SUBMISSION_LIMIT):
-            logger.debug("Parsing submission %s", sub)
-            handle_submission(sub)
-        time.sleep(PROCESSING_INTERVAL)
+    def __main_loop(self):
+        logger.info("Main loop started")
+        while True:
+            # parse the last 25 submissions in /r/finanzen
+            for sub in self.__subreddit.new(limit=SUBMISSION_LIMIT):
+                logger.debug("Parsing submission %s", sub)
+                self.__handle_submission(sub)
+            time.sleep(PROCESSING_INTERVAL)
+
+    def start(self):
+        self.__setup_reddit()
+        self.__main_loop()
 
 
 if __name__ == "__main__":
-    setup_reddit()
-    main_loop()
+    bot = RedditWertpapierBot()
+    bot.start()
     # handle_stock_requests("abc", ["ETF110", "FR0010315770", "LU0468897110"])
